189f408996817f0f917324b112f6fae36aef6969
SPS <123806275+SurajPratapSingh2022@users.noreply.github.com>
2024-12-30 09:14:59 +0530

    Delete java directory

--- java/sumOf2NumCLA.java
+++
@@ -1,9 +0,0 @@
-//Sum of 2 number using CLA in Java
-class Main {
-  public static void main(String[] args)
-  {
-    int n1=Integer.parseInt(args[0]);
-    int n2=Integer.parseInt(args[1]);
-    System.out.println(n1+n2);
-  }
-}
